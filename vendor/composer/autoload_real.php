3a91e5bd46549e23ba5b1804eb45359b7cb9c516
Frank Cordoba <cordobafrank98@gmail.com>
2021-09-02 22:01:40 -0500

    Add Pusher

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,24 +13,19 @@ class ComposerAutoloaderInita7ab092ad4d6716a473ec3192b0763dd
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInita7ab092ad4d6716a473ec3192b0763dd', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInita7ab092ad4d6716a473ec3192b0763dd', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInita7ab092ad4d6716a473ec3192b0763dd::getInitializer($loader));
         } else {
